1c7c1dabc22646f4be64a439eb88fa03e9f880e5
SeuneKeun <eiziong1@gmail.com>
2025-01-23 16:47:39 +0900

    refactor: Spring Security 도입
    - 기존 Filter, ArgumentResolver -> Security로 변경
    - AuthUser를 그대로 사용하기 위해 UserDetails를 구현받아 사용
    - @Auth -> @AuthenticationPrincipal로 변경

--- src/main/java/org/example/expert/domain/common/dto/AuthUser.java
+++ src/main/java/org/example/expert/domain/common/dto/AuthUser.java
@@ -1,20 +1,55 @@
 package org.example.expert.domain.common.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.example.expert.domain.user.enums.UserRole;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
 
 @Getter
-public class AuthUser {
+@AllArgsConstructor
+public class AuthUser implements UserDetails {
 
     private final Long id;
     private final String email;
     private final String nickname;
     private final UserRole userRole;
 
-    public AuthUser(Long id, String email, String nickname, UserRole userRole) {
-        this.id = id;
-        this.email = email;
-        this.nickname = nickname;
-        this.userRole = userRole;
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton(() -> "ROLE_" + userRole.name());
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
